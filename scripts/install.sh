38b932b384c737d4b224944658b9b2e311f37953
Adam Aposhian <aposhian.dev@gmail.com>
2021-05-04 12:52:15 -0600

    added rosdep

--- scripts/install.sh
+++ scripts/install.sh
@@ -322,8 +322,13 @@ sudo apt install -yqq ros-noetic-desktop-full
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt-get update
-sudo apt-get install -yqq ros-foxy-desktop python3-colcon-common-extensions
+sudo apt-get install -yqq \
+	ros-foxy-desktop \
+	python3-colcon-common-extensions \
+	python3-rosdep
 python3 -m pip install -U argcomplete
+sudo rosdep init
+rosdep update
 
 ############################
 ###### Customization #######
